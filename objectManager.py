5ca8366575bf0ceecbf60236f16dc8d1d8d31415
DavidGrunheidt <david.ordine@grad.ufsc.br>
2019-09-04 17:53:00 -0300

    Create objectManager

---
+++ objectManager.py
@@ -0,0 +1,16 @@
+from object Import Object
+
+#2D Window and Viewport starts with 0 as all coordinates default values.
+window = (0, 0, 0, 0)
+viewport = (0, 0, 0, 0)
+
+#List of objects.
+display_file = []
+
+def set_window(xw_min: Float, yw_min: Float, xw_max: Float, yw_max: Float):
+	window = (xw_min, yw_min, xw_max, yw_max)
+
+
+
+
+
