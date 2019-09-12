cf8ff41a2a24238c61e0e6c3ee000ab1fed46237
DavidGrunheidt <david.ordine@grad.ufsc.br>
2019-09-12 08:28:48 -0300

    Transformada viewport 2D

--- objectManager.py
+++ objectManager.py
@@ -1,22 +1,106 @@
 from object import Object
 
 #2D Window and Viewport starts with 0 as all coordinates default values.
-window = (0.0, 0.0, 0.0, 0.0)
-viewport = (0.0, 0.0, 0.0, 0.0)
+window = {
+	"xWinMin": 0.0, 
+	"yWinMin": 0.0, 
+	"xWinMax": 0.0, 
+	"yWinMax": 0.0,
+	"xDif": 0.0,
+	"yDif": 0.0
+	}
+
+viewport = {
+	"xVpMin": 0.0,
+	"yVpMin": 0.0, 
+	"xVpMax": 0.0, 
+	"yVpMax": 0.0,
+	"xDif": 0.0,
+	"yDif": 0.0
+	}
 
 #List of objects.
 display_file: dict = {}
 
-def set_window(xw_min: float, yw_min: float, xw_max: float, yw_max: float) -> bool:
-	window = (xw_min, yw_min, xw_max, yw_max)
+def create_new_object(name: str, coordinates: 'string containing triples of x,y,z coordinates splitted by a ";"') -> Object :
+	coordinates_matrix = []
+	index_row = 0
+	for triple in coordinates.split(';'):
+		coordinates = triple.split(',')
+		try:
+			coordinates = [float(x) for x in coordinates]
+		except ValueError:
+			raise ValueError("Insira coordenadas validas. \n Coordenadas devem todas ser duplas de números inteiros ou decimais.")
 
-def set_viewport(xvp_min: float, yvp_min: float, vxp_max: float, yvp_max: float) -> bool:
-	viewport = (xvp_min, yvp_min, xvp_max, yvp_max)
+		coordinates_matrix.append([])
 
-def create_new_object(name: str, coordinates: 'list of [x,y,z] coordinates (matrix)'):
-	newObject = Object(name, coordinates)
+		if (len(coordinates) == 2 or len(coordinates) == 3):
+			for coordinate in coordinates:
+				coordinates_matrix[index_row].append(coordinate)
+
+			if (len(coordinates) == 2):
+				coordinates_matrix[index_row].append(0)
+
+			index_row += 1
+		else:
+			raise ValueError("Coordenadas devem ser duplas ou triplas")
+
+	newObject = Object(name, coordinates_matrix)
 	display_file[name] = newObject
 
+	#For debug purpose
+	print("Objeto "+"\""+newObject.name+"\" ("+newObject.type+") criado nas seguintes coordenadas = "+str(newObject.coordinates)+".\nDisplay File com "+str(len(display_file))+" objeto(s)") 
+
+	return newObject
+
+def viewport_transform(coordinates):
+	coordinates_on_viewport = []
+	index_row = 0
+
+	for triple in coordinates:
+		coordinates_on_viewport.append([])
+
+		xw = triple[0]
+		yw = triple[1]
+
+		xVp = ((xw - window["xWinMin"]) / window["xDif"]) * viewport["xDif"]
+
+		yVp = (1 - ((yw - window["yWinMin"]) / window["yDif"])) * viewport["yDif"]
+
+		coordinates_on_viewport[index_row].append(xVp)
+		coordinates_on_viewport[index_row].append(yVp)
+
+		index_row += 1
+
+	#For debug purpose
+	print("Coordenadas do objeto na viewport = "+str(coordinates_on_viewport)+"\n")
+
+	return coordinates_on_viewport
+
+def set_window(xWinMin: float, yWinMin: float, xWinMax: float, yWinMax: float) -> None:
+	global window
+	window["xWinMin"] = xWinMin
+	window["yWinMin"] = yWinMin
+	window["xWinMax"] = xWinMax
+	window["yWinMax"] = yWinMax
+	window["xDif"] = xWinMax - xWinMin
+	window["yDif"] = yWinMax - yWinMin
+
+
+def set_viewport(xVpMin: float, yVpMin: float, xVpMax: float, yVpMax: float) -> None:
+	global viewport
+	viewport["xVpMin"] = xVpMin
+	viewport["yVpMin"] = yVpMin
+	viewport["xVpMax"] = xVpMax
+	viewport["yVpMax"] = yVpMax
+	viewport["xDif"] = xVpMax - xVpMin
+	viewport["yDif"] = yVpMax - yVpMin
+
+def get_window() -> (float, float, float, float):
+	return window
+
+def get_viewport() -> (float, float, float, float):
+	return viewport
 
 
 
--- screenManager.py
+++ screenManager.py
@@ -1,4 +1,3 @@
-import objectManager
 import cairo
 import gi
 gi.require_version('Gtk', '3.0')
@@ -7,10 +6,7 @@ from objectManager import *
 
 surface = None
 
-x = 10
-y = 30
-
-def cgi_init():
+def cgi_init() -> None:
     global gtkBuilder, window_widget, drawing_area, surface, object_list
 
     gtkBuilder = Gtk.Builder()
@@ -30,8 +26,34 @@ def cgi_init():
     window_widget.show_all()
     Gtk.main()
 
+def show_new_object(obj: Object) -> None:
+    global gtkBuilder, object_list, surface
+
+    listed_object = gtkBuilder.get_object('generic_object_button').new_with_label(obj.name+" ("+obj.type+")")
+    object_list.insert(listed_object, -1)
+    object_list.show_all()
+
+    coordinates_on_viewport = viewport_transform(obj.coordinates)
+
+    cr = cairo.Context(surface)
+    cr.move_to(coordinates_on_viewport[0][0], coordinates_on_viewport[0][1])
+
+    if (obj.isPoint):
+        cr.line_to(coordinates_on_viewport[0][0]+1, coordinates_on_viewport[0][1]+1)
+    else:
+        index_row = 0
+        index_column = 0
+        for coordinates in coordinates_on_viewport[1:len(coordinates_on_viewport)]:
+            cr.line_to(coordinates[0], coordinates[1])
+        #cr.line_to(coordinates_on_viewport[0][0], coordinates_on_viewport[0][1])
+        cr.close_path() #Se não puder usar esse usa o de cima pra fechar o poligono.
+
+    cr.stroke()
+    window_widget.queue_draw()
+
+
 # Clear the surface, removing the scribbles
-def clear_surface():
+def clear_surface() -> None:
     global surface
     cr = cairo.Context(surface)
     cr.set_source_rgb(1, 1, 1)
@@ -39,11 +61,13 @@ def clear_surface():
     del cr
 
 # Creates the surface
-def configure_event_cb(wid, evt):
+def configure_event_cb(wid, evt) -> None:
     global surface, width, height
+
     if surface is not None:
         del surface
         surface = None
+
     win = wid.get_window()
     width = wid.get_allocated_width()
     height = wid.get_allocated_height()
@@ -51,34 +75,46 @@ def configure_event_cb(wid, evt):
         cairo.CONTENT_COLOR,
         width,
         height)
+
+    set_window(0, 0, width, height)
+    set_viewport(0, 0, width, height)
+
     clear_surface()
+
     return True
 
 # Redraw the screen from the surface
-def draw_cb(wid, cr):
+def draw_cb(wid, cr) -> None:
     global surface
     cr.set_source_surface(surface, 0, 0)
     cr.paint()
     return False
 
-def show_error(message: str):
+def show_error(message: str) -> None:
     gtkBuilder.add_from_file('showError.glade')
+
     errorWindow = gtkBuilder.get_object('window_show_error')
     errorWindow.set_transient_for(dialog_newObj)
     errorWindow.set_modal(True)
+
     errorLabel = gtkBuilder.get_object('label_error')
     errorLabel.set_text(message)
+
     errorWindow.show_all()
 
 class Handler:
     # Function that will be called when the ok button is pressed
-    def new_obj_clicked(self, widget):
+    def new_obj_clicked(self, widget) -> None:
         global dialog_newObj, gtkBuilder
+
         gtkBuilder.add_from_file('dialogNewObj.glade')
+
         dialog_newObj = gtkBuilder.get_object('dialog_new_obj')
         dialog_newObj.set_transient_for(window_widget)
         dialog_newObj.set_modal(True)
+
         gtkBuilder.connect_signals(Handler())
+
         dialog_newObj.show_all()
     
     def btn_rotateX_toggled(self,btn):
@@ -108,76 +144,29 @@ class Handler:
     def window_moveLeft_clicked(self,btn):
         pass 
 
-    def cofirm_new_obj(self, btn):
-        global dialog_newObj, gtkBuilder, object_list
+    def cofirm_new_obj(self, btn) -> None:
+        global dialog_newObj, gtkBuilder
+
         newObj_coordinates_raw = gtkBuilder.get_object('object_coordinates_entry').get_text()
-        newObj_coordinates = newObj_coordinates_raw.split(';')
+        newObj_name = gtkBuilder.get_object('newObj_name_entry').get_text()
 
-        object_name = gtkBuilder.get_object('object_name_entry').get_text()
-
-        if (object_name in display_file):
+        if (newObj_name in display_file):
             show_error("Nome já definido, escolha outro nome.")
         else:
-            error = False
+            newObj = None
 
-            coordinates_matrix = []
-            index_row = 0
-            for triple in newObj_coordinates:
-                coordinates = triple.split(',')
+            try: 
+                newObj = create_new_object(newObj_name, newObj_coordinates_raw)
+            except ValueError as e:
+                show_error(str(e))
+                return
 
-                try:
-                    coordinates = [float(x) for x in coordinates]
-                except ValueError:
-                    show_error("Insira coordenadas validas. \n Coordenadas devem todas ser duplas de números inteiros ou decimais.")
-                    error = True
-                    break
+            show_new_object(newObj)
 
-                coordinates_matrix.append([])
-
-                if (len(coordinates) == 2 or len(coordinates) == 3):
-                    for coordinate in coordinates:
-                        coordinates_matrix[index_row].append(coordinate)
-
-                    if (len(coordinates) == 2):
-                        coordinates_matrix[index_row].append(0)
-
-                    index_row += 1
-                else:
-                    show_error("Coordenadas devem ser duplas ou triplas")
-                    error = True
-                    break
-
-            if (not error):
-                newObj = Object(object_name, coordinates_matrix)
-                display_file[object_name] = newObj
-
-                listed_object = gtkBuilder.get_object('generic_object_button').new_with_label(object_name+" ("+newObj.type+")")
-                object_list.insert(listed_object, -1)
-                object_list.show_all()
-
-                cr = cairo.Context(surface)
-                cr.move_to(coordinates_matrix[0][0], coordinates_matrix[0][1])
-
-                if (newObj.isPoint):
-                    cr.line_to(coordinates_matrix[0][0]+1, coordinates_matrix[0][1]+1)
-                else:
-                    index_row = 0
-                    index_column = 0
-                    for coordinates in coordinates_matrix[1:len(coordinates_matrix)]:
-                        cr.line_to(coordinates[0], coordinates[1])
-                    #cr.line_to(coordinates_matrix[0][0], coordinates_matrix[0][1])
-                    cr.close_path() #Se não puder usar esse usa o de cima pra fechar o poligono.
-
-                cr.stroke()
-                window_widget.queue_draw()
-
-                dialog_newObj.hide()
-
-                #For debug purpose
-                print("Objeto "+"\""+object_name+"\" ("+newObj.type+") criado nas seguintes coordenadas = "+str(coordinates_matrix)+". Display File com "+str(len(display_file))+" objeto(s)") 
+            dialog_newObj.hide()
 
 
-    def cancel_new_obj(self, btn):
+    def cancel_new_obj(self, btn) -> None:
         global dialog_new_obj
         dialog_newObj.hide()
 
